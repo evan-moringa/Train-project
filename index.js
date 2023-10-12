4b07596f929cced0ce77435d7b66cf4cb6efb163
evan keya <evan.keya@student.moringaschool.com>
2023-10-12 23:25:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-//fetch ('http://localhost:3000/trains/')
+fetch(`http://localhost:3000/trains/`)
+.then (res => res.json)
+.then (api_Url => {
+    console.log(api_Url);
+    fetchAndDisplayTrainInfo(data)
+})
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const trainInfoElement = document.getElementById("train-info");
 
     // Replace with the URL of your train information API
-    const api_Url = "https://localhost:3000/trains/";
+    const api_Url = `http://localhost:3000/trains/`;
 
     // Function to fetch and display train information
     function fetchAndDisplayTrainInfo() {
@@ -56,6 +63,62 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initial data load when the page loads
     fetchAndDisplayTrainInfo(apiUrl);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const trainInfoElement = document.getElementById("train-info");
+    const loadDataButton = document.getElementById("load-data-button");
+
+    // Replace with the URL of your train information API
+    const apiUrl = `http://localhost:3000/trains/${data.trains_id}`;
+
+    // Function to fetch and display train information
+    function fetchAndDisplayTrainInfo(apiUrl) {
+        fetch(apiUrl)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const trainData = data.trains;
+
+                if (trainData.length === 0) {
+                    trainInfoElement.textContent = "No train information available.";
+                } else {
+                    trainInfoElement.innerHTML = ""; // Clear previous data
+
+                    trainData.forEach((train) => {
+                        const trainDiv = document.createElement("div");
+                        trainDiv.className = "train-info";
+
+                        const trainName = document.createElement("h2");
+                        trainName.textContent = `Train: ${train.train_name}`;
+
+                        const departureTime = document.createElement("p");
+                        departureTime.textContent = `Departure Time: ${train.departure_time}`;
+
+                        const arrivalTime = document.createElement("p");
+                        arrivalTime.textContent = `Arrival Time: ${train.arrival_time}`;
+
+                        trainDiv.appendChild(trainName);
+                        trainDiv.appendChild(departureTime);
+                        trainDiv.appendChild(arrivalTime);
+
+                        trainInfoElement.appendChild(trainDiv);
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching train data:", error);
+                trainInfoElement.textContent = "Error fetching train data.";
+            });
+    }
+
+    // Add a click event listener to the button
+    loadDataButton.addEventListener("click", fetchAndDisplayTrainInfo);
+});
+
 
 
 
